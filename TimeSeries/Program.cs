5196401334effdd3715d957541b0ef8891f64402
Fitzchak Yitzchaki <fitzchak@ayende.com>
2015-05-03 17:17:36 +0300

    Commit initial version

---
+++ TimeSeries/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using Voron;
+
+namespace TimeSeries
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+			using (var tss = new TimeSeriesStorage(StorageEnvironmentOptions.CreateMemoryOnly()))
+			{
+				var sp = Stopwatch.StartNew();
+				
+				var count = 5;
+				var now = DateTime.Now;
+				while (sp.ElapsedMilliseconds < 10 * 1000)
+				{
+					using (var w = tss.CreateWriter())
+					{
+						for (int i = 0; i < 100; i++)
+						{
+							now = now.AddMinutes(1);
+							w.Append("one", now, i);
+							w.Append("two", now, i);
+							w.Append("three", now, i);
+							w.Append("four", now, i);
+							w.Append("five", now, i);
+							count += 5;
+						}
+
+						w.Commit();
+					}
+				}
+
+				sp.Stop();
+				Console.WriteLine(sp.Elapsed);
+				Console.WriteLine("{0:#,#}", count);
+				Console.WriteLine(Math.Round((double)count/ sp.ElapsedMilliseconds, 4));
+
+				/*using (var r = tss.CreateReader())
+				{
+					foreach (var point in r.Query("1234", DateTime.Now.AddDays(-1), DateTime.Now.AddMinutes(5)))
+					{
+						Console.WriteLine(point.At + " " + point.Value);
+					}
+				}*/
+			}
+		}
+	}
+}
---
+++ TimeSeries/TimeSeriesStorage.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Voron;
+using Voron.Impl;
+using Voron.Util.Conversion;
+
+namespace TimeSeries
+{
+	public class TimeSeriesStorage : IDisposable
+	{
+		private readonly StorageEnvironment _storageEnvironment;
+
+		public Guid Id { get; set; }
+
+		public TimeSeriesStorage(StorageEnvironmentOptions options)
+		{
+			_storageEnvironment = new StorageEnvironment(options);
+
+			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				var metadata = _storageEnvironment.CreateTree(tx, "$metadata");
+				var result = metadata.Read("id");
+				if (result == null) // new db
+				{
+					Id = Guid.NewGuid();
+					metadata.Add("id", new MemoryStream(Id.ToByteArray()));
+				}
+				else
+				{
+					int used;
+					Id = new Guid(result.Reader.ReadBytes(16, out used));
+				}
+			}
+		}
+
+		public Reader CreateReader()
+		{
+			return new Reader(this);
+		}
+
+		public Writer CreateWriter()
+		{
+			return new Writer(this);
+		}
+
+		public class Point
+		{
+			public DateTime At { get; set; }
+
+			public double Value { get; set; }
+		}
+
+		public class Reader : IDisposable
+		{
+			private readonly TimeSeriesStorage _storage;
+			private readonly Transaction _tx;
+
+			public Reader(TimeSeriesStorage storage)
+			{
+				_storage = storage;
+				_tx = _storage._storageEnvironment.NewTransaction(TransactionFlags.Read);
+			}
+
+			public IEnumerable<Point> Query(string treeName, DateTime start, DateTime end)
+			{
+				var startBuffer = EndianBitConverter.Big.GetBytes(start.Ticks);
+				var endBuffer = EndianBitConverter.Big.GetBytes(end.Ticks);
+
+				var buffer = new byte[8];
+
+				var tree = _tx.State.GetTree(_tx, treeName);
+				using (var it = tree.Iterate())
+				{
+					it.MaxKey = new Slice(endBuffer);
+					if (it.Seek(new Slice(startBuffer)) == false)
+						yield break;
+
+					do
+					{
+						var reader = it.CreateReaderForCurrent();
+						reader.Read(buffer, 0, 8);
+
+						yield return new Point
+						{
+							At = new DateTime(it.CurrentKey.CreateReader().ReadBigEndianInt64()),
+							Value = EndianBitConverter.Big.ToDouble(buffer, 0)
+						};
+					} while (it.MoveNext());
+				}
+			}
+
+			public void Dispose()
+			{
+				if (_tx != null)
+					_tx.Dispose();
+			}
+		}
+
+
+		public class Writer : IDisposable
+		{
+			private readonly TimeSeriesStorage _storage;
+			private Transaction _tx;
+
+			public Writer(TimeSeriesStorage storage)
+			{
+				_storage = storage;
+				_tx = _storage._storageEnvironment.NewTransaction(TransactionFlags.ReadWrite);
+			}
+
+			public void Append(string treeName, DateTime time, double value)
+			{
+				var tree = _tx.State.GetTree(_tx, treeName);
+				var key = EndianBitConverter.Big.GetBytes(time.Ticks);
+				var valueAsBytes = EndianBitConverter.Big.GetBytes(value);
+
+				tree.Add(new Slice(key), new MemoryStream(valueAsBytes));
+			}
+
+			public void Dispose()
+			{
+				if (_tx != null)
+					_tx.Dispose();
+			}
+
+			public void Commit()
+			{
+				_tx.Commit();
+			}
+		}
+
+		public void Dispose()
+		{
+			if (_storageEnvironment != null)
+				_storageEnvironment.Dispose();
+		}
+	}
+}
